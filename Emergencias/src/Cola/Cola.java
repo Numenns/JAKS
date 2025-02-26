65c615db77414615b0ffb6ad7487ec6514d6e567
Numenns <juandavidar0212@gmail.com>
2025-02-25 23:57:04 -0500

    Add files via upload
    esta vuelta es la libreria cola, en ella esta la data Emergencia

---
+++ Emergencias/src/Cola/Cola.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cola;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author David Castro
+ */
+public class Cola<T extends Base> {
+
+    private LinkedList<T> cola;
+
+    public Cola() {
+        cola = new LinkedList<>();
+    }
+
+    public void encolar(T elemento) {
+        cola.addLast(elemento);
+    }
+
+    public T desencolar() {
+        return cola.removeFirst();
+    }
+
+    public boolean estaVacia() {
+        return cola.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (T e : cola) {
+            sb.append(" ").append(e.toString());
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
